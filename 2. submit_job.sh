66f5fe0c66ce2213c884f62352e2ee160b787746
Tae Young Yeon <50289876+taeyoungyeon@users.noreply.github.com>
2025-02-03 23:41:52 -0600

    Update 2. submit_job.sh

--- 2. submit_job.sh
+++ 2. submit_job.sh
@@ -4,16 +4,16 @@
 #SBATCH --job-name=quest_checkpoint
 #SBATCH --output=checkpoint_job.log
 #SBATCH --error=checkpoint_job.err
-#SBATCH --time=02:00:00   # Set maximum job run time (hh:mm:ss)
+
+# partition has several attributes. for training models, you should use gregpu for it. 
+# refer https://services.northwestern.edu/TDClient/30/Portal/KB/ArticleDet?ID=1549 for details.
+#SBATCH --time=02:00:00   
 #SBATCH --partition=short
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 
-# Load necessary modules (adjust as needed)
-module load python/3.8
-
-# If using a Conda environment, uncomment and modify the next line:
-# source ~/anaconda3/bin/activate quest_env
+# If using a Conda environment or settings, uncomment and modify the next line:
+# conda activate envname
 
 # Run the Python script for checkpointing
 python3 1. training_with_checkpoint.py
